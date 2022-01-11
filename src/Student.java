777960f6c2530d5f1b8886617d154304cd5c3c1a
1ArekKreft <arkadiuszkreft32@gmail.com>
2022-01-11 19:41:02 +0100

    init

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    protected String Imie;
+    protected String Nazwisko;
+    protected int Wiek;
+    protected boolean CzyLubiLabolatoriaZJavy;
+
+    Student(String i, String n, int w, boolean c)
+    {
+        this.Imie = i;
+        this.Nazwisko = n;
+        this.Wiek = w;
+        this.CzyLubiLabolatoriaZJavy = c;
+    }
+}
